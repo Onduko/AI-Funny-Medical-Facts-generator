9b4d5ae0ccc7ed0acde7e57b4605211d04cb58ea
Onduko <ondukodaisy@gmail.com>
2025-03-18 18:17:03 +0300

    Paragraph inner html changed

--- src/script.js
+++ src/script.js
@@ -3,6 +3,11 @@ let form = document.querySelector("form");
 let fact = document.querySelector("#fact");
 function getInput(event) {
   event.preventDefault();
-  console.log(organInput.value);
+  new Typewriter("#fact", {
+    strings: `${organInput.value}`,
+    autoStart: true,
+    delay: 40,
+    cursor: "",
+  });
 }
 addEventListener("submit", getInput);
